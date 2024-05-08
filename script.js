db0391ffb0d29ce1fd47c03452228d0edf0cd8ef
Mschlei-48 <estermakade@gmail.com>
2024-05-08 15:20:36 +0200

    intro-to-javascript

---
+++ script.js
@@ -0,0 +1,11 @@
+let name="Mishi Makade";
+let bio="Hardworking and dedicated individual with good communication skills. Always eager to learn and expand knowledge. As a result of my excellent C++ skills and good communication skills, I tutored first years in Data Structures and Algorithms in the C++ programming language module. Very skilled in two programming languages, Python and C++. Familiar with Tableau data visualization software, M ATLAB, Javascript, and SQL database management and development.";
+let mobile_number="+27 73 894 7110";
+let email="mishimakade48@gmail.com";
+let high_school="Fundani High School (2014 - 2018)";
+let tertiary1="Sol Plaatje University - BSc Data Science (2019 - 2021)";
+let tertiary2="Sol Plaatje University - BSc Hons Data Science  (2023-present)";
+let projects=["Web Scraping","Suicide Detection","Titanic Data Exploration, Preparation, and Modeling","Introduction to Django"];
+let experience_workplace={"Data Structures and Algorithms tutor":"Sol Plaatje University (07/2020 - 11/2020)",
+                            "Big Data Foundantions Tutor":"Sol Plaatje University (07/2023 - 11/2023)",
+                        "Library Monitor":"Sol Plaatje University (07/2023 - 11/2023)"};
